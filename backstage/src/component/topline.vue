263b34c747da3e71408699383c6fed40e96e19b1
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-04-27 14:56:36 +0800

    上传导航栏

---
+++ backstage/src/component/topline.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="topline">
+    <a  class="title">菜购超市后台</a>
+    <a class="name">退出</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"topline"
+}
+</script>
+
+<style>
+    .topline{
+        width: 100%;
+        height : 70px;
+        background: #2379EB;
+        border: none;
+        font-size:20px;
+        
+    }
+    .title{
+        position: relative;
+        color: ivory;
+        left:-650px;
+        top:30%
+    }
+    .name{
+        position: relative;
+        color: ivory;
+        left: 42%;
+        top: 30%;
+    }
+</style>
