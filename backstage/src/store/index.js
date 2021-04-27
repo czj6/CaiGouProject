446173cf57e171b0f1f8ab8b854eeea282cc3492
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-04-27 14:33:58 +0800

    Update index.js

--- backstage/src/store/index.js
+++ backstage/src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Vuex)
+import { Button, Input, TableColumn, Table, Switch, Menu, RadioButton, Card, Radio,RadioGroup,MenuItem,Form,Checkbox,CheckboxGroup,Dialog,Tabs } from 'element-ui'
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+Vue.config.productionTip = false
+Vue.component(Button.name, Button)
+Vue.component(Input.name, Input)
+Vue.component(TableColumn.name, TableColumn)
+Vue.component(Table.name, Table)
+Vue.component(Switch.name, Switch)
+Vue.component(Menu.name, Menu)
+Vue.component(RadioButton.name, RadioButton)
+Vue.component(Card.name, Card)
+Vue.component(Radio.name, Radio)
+Vue.component(RadioGroup.name,RadioGroup)
+Vue.component(MenuItem.name,MenuItem)
+Vue.component(Form.name,Form)
+Vue.component(Checkbox.name,Checkbox)
+Vue.component(CheckboxGroup.name,CheckboxGroup)
+Vue.component(Dialog.name,Dialog)
+Vue.component(Tabs.name, Tabs)
+
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
