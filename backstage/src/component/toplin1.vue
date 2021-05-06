fc05045289e104770d517ce3f312263fb07244c1
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-05-06 16:09:42 +0800

    Create toplin1.vue

---
+++ backstage/src/component/toplin1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="toplin1">
+    <a  class="titl">订单详情</a>
+    <a class="name">退出</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"toplin1"
+}
+</script>
+
+<style>
+    .toplin1{
+        width: 100%;
+        height : 70px;
+        background: #2379EB;
+        border: none;
+        font-size:20px;
+        
+    }
+    .titl{
+        position: relative;
+        color: ivory;
+        left:0px;
+        top:30%
+    }
+    .name{
+        position: relative;
+        color: ivory;
+        left: 42%;
+        top: 30%;
+    }
+</style>
